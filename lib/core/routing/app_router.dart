7965fdf8d9d911c87aa37b4fd6107e55182893df
gulebest <gulumabest694@gmail.com>
2025-12-31 23:50:54 +0300

    l

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -16,7 +16,7 @@ import '../../features/onboarding/presentation/onboarding_page_1.dart';
 import '../../features/onboarding/presentation/onboarding_page_2.dart';
 import '../../features/onboarding/presentation/onboarding_page_3.dart';
 
-/// 🔁 Refresh router when auth state changes
+///  Refresh router when auth state changes
 class GoRouterRefreshStream extends ChangeNotifier {
   late final StreamSubscription _sub;
 
