8b725eefec8fcb83c8ed5867c6ed747df3f430ac
Guluma Tafa <gulumabest694@gmail.com>
2026-01-08 20:53:57 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // ✅ Firebase initialization (Web + Mobile safe)
+  // ✅ Firebase initialization 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(
