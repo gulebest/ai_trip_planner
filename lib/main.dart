1f47d372a23bf9a7c256c048e044d3ba0ec1781d
Guluma Tafa <gulumabest694@gmail.com>
2026-01-15 20:15:16 +0300

    Update Firebase initialization comment

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // ✅ Firebase initialization 
+  // ✅ Firebase  
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(
