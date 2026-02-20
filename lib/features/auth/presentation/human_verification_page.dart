d0f3d1cc37fae448bd8cb7ccc259036036952668
Guluma Tafa <gulumabest694@gmail.com>
2026-02-20 14:02:12 +0300

    Fix typo in comment for checkbox container

--- lib/features/auth/presentation/human_verification_page.dart
+++ lib/features/auth/presentation/human_verification_page.dart
@@ -52,7 +52,7 @@ class _HumanVerificationPageState extends State<HumanVerificationPage> {
 
             const SizedBox(height: 32),
 
-            /// Checkbox container
+            /// Checkbox cont
             Container(
               padding: const EdgeInsets.all(16),
               decoration: BoxDecoration(
