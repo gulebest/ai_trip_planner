2418301a10b1e4d0424c188df44965982df27796
Guluma Tafa <gulumabest694@gmail.com>
2026-03-09 09:34:35 +0300

    Fix comment typo in home_screen.dart

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:go_router/go_router.dart';
-// This is home pa
+// This is home p
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
