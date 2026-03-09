aef91054ff521178b74067e5cf7841667607bd00
Guluma Tafa <gulumabest694@gmail.com>
2026-03-09 09:34:44 +0300

    Fix comment in home_screen.dart

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:go_router/go_router.dart';
-// This is home p
+// This is home 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
